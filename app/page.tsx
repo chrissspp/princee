9f2f49326b77ff57854b6b02b6e93d346872cfc8
chrissspp <christianjprince12@gmail.com>
2025-04-30 13:52:33 -0400

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,1 @@
-<!DOCTYPE html>
-<html>
-<head>
-<title>Hello</title>
-</head>
-<body>
-
-<h1>Hello</h1>
-
-<p>This is a simple HTML page that displays the word "Hello".</p>
-
-</body>
-</html>
+console.log("Hello");
